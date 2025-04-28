eee43010026455ae05b04db2700e394d29a5f88b
RayLLLLLL <44373899+rayltitan1993@users.noreply.github.com>
2025-04-28 15:30:37 +0800

    Update header.tsx

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -23,8 +23,8 @@ export default function Header() {
         <Image
           src="/assets/logo.png"  // 图片路径，需要将图片放在public/assets目录下
           alt="Blog Logo"
-          width={80}  // 根据你的图片和设计需求调整宽度
-          height={79}  // 根据你的图片和设计需求调整高度
+          width={60}  // 根据你的图片和设计需求调整宽度
+          height={60}  // 根据你的图片和设计需求调整高度
           className="mr-2"  // 添加右侧边距，使布局美观
         />
         <span className="hidden md:inline">| Home</span>
