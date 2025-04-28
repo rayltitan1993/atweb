5a617811a3fab04307a42a59740283d545674d26
RayLLLLLL <44373899+rayltitan1993@users.noreply.github.com>
2025-04-28 15:26:37 +0800

    Update intro.tsx

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -1,20 +1,21 @@
 import { CMS_NAME } from "@/lib/constants";
+import Image from 'next/image'
+import Link from 'next/link'
 
 export function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Blog.
-      </h1>
+      <Link href="/" className="hover:underline flex items-center">
+        <Image
+          src="/assets/logobig.png"  // 图片路径，需要将图片放在public/assets目录下
+          alt="Blog Logo"
+          width={315}  // 根据你的图片和设计需求调整宽度
+          height={60}  // 根据你的图片和设计需求调整高度
+          className="mr-2"  // 添加右侧边距，使布局美观
+        />
+      </Link>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog example using{" "}
-        <a
-          href="https://nextjs.org/"
-          className="underline hover:text-blue-600 duration-200 transition-colors"
-        >
-          Next.js
-        </a>{" "}
-        and {CMS_NAME}.
+        Professional | Passion 
       </h4>
     </section>
   );
