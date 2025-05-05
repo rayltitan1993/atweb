665fed94c070cc894739f48beb55d012ded428b5
RayLLLLLL <44373899+rayltitan1993@users.noreply.github.com>
2025-05-05 11:25:44 +0800

    Update footer.tsx

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -11,16 +11,15 @@ export function Footer() {
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
-              href="https://www.google.com"
+              href="mailto: rayl@gmail.com"
               className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
             >
               Contact Us
             </a>
             <a
               href={``}
-              className="mx-3 font-bold hover:underline"
+              className="mx-3 font-bold hover:nounderline"
             >
-              Address:
               Nyuphenburgerstr.12, Munich, Germany 
             </a>
           </div>
