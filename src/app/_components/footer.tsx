2c18d1615fcee584b4eef40c9bda4376cbbef598
RayLLLLLL <44373899+rayltitan1993@users.noreply.github.com>
2025-05-05 19:29:16 +0800

    Update footer.tsx

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -12,7 +12,7 @@ export function Footer() {
           </h3> 
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
-              href="mailto: rayl@gmail.com"
+              href="mailto: zijian.ding@aternastech.com"
               className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
             >
               Contact Us
