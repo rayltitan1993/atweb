bfa957292b9c27940527b01bfb0a77ea0bc63018
RayLLLLLL <44373899+rayltitan1993@users.noreply.github.com>
2025-05-05 11:57:43 +0800

    Update footer.tsx

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -8,6 +8,10 @@ export function Footer() {
         <div className="py-28 flex flex-col lg:flex-row items-center">
           <h3 className="text-4xl lg:text-[2.0rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
             Aternas - Professional Service Provider
+          </h3> 
+          <br />
+          <h3 className="text-xl lg:text-[2.0rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
+            @All Rights Reserved.
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
